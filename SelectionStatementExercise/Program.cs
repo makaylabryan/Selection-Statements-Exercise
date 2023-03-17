aac1fc70eb5351375f67212bda5ecc983ac24ba1
Makayla Bryan <makaylabryan@gmail.com>
2023-03-17 11:58:16 -0400

    what is my favorite number

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace SelectionStatementExercise
+﻿using System.ComponentModel.Design;
+
+namespace SelectionStatementExercise
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var r = new Random();
+            var favoriteNumber = r.Next(1, 1000);
+           
+            Console.WriteLine("Try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favoriteNumber)
+            {
+                Console.WriteLine("Too low");
+            }
+            else if(userInput > favoriteNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+            else
+            {
+                Console.WriteLine("Great job!");
+            }
         }
     }
 }
